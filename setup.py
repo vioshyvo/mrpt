93af17791f9d3a06dbd7cf9fc44837cc281d0fb0
ejaasaari <elias.jaasaari@gmail.com>
2017-07-12 11:31:30 +0300

    OSX support

--- setup.py
+++ setup.py
@@ -6,29 +6,31 @@ from setuptools import Extension
 
 # Not all CPUs have march as a tuning parameter
 import platform
-cputune = ['-march=native',]
-if platform.machine() == "ppc64le":
-    cputune = ['-mcpu=native',]
+cputune, llvm = ['-march=native'], []
+if platform.machine() == 'ppc64le':
+    cputune = ['-mcpu=native']
+if platform.system() == 'Darwin':
+    llvm += ['-L/usr/local/opt/llvm/lib']
 
 setuptools.setup(
     name='mrpt',
     version='0.1',
     url='http://github.com/teemupitkanen/mrpt',
     install_requires=[],
-    packages={ '.': 'mrpt' },
+    packages={'.': 'mrpt'},
     zip_safe=False,
     test_suite='py.test',
     entry_points='',
-    ext_modules = [
+    ext_modules=[
         Extension('mrptlib',
-            sources = [
+            sources=[
                 'cpp/mrptmodule.cpp',
             ],
             extra_compile_args=['-std=c++11', '-O3', '-ffast-math', '-s',
                                 '-fno-rtti', '-fopenmp', '-DNDEBUG'] + cputune,
-            extra_link_args=['-lgomp'],
-            libraries = ['stdc++'],
-            include_dirs = ['cpp/lib', numpy.get_include()]
+            libraries=['stdc++'],
+            extra_link_args=['-lgomp'] + llvm,
+            include_dirs=['cpp/lib', numpy.get_include()]
         )
     ]
 )
