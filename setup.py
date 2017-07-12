b767b8141377d289948e45cfca157d7c423f0873
ejaasaari <elias.jaasaari@gmail.com>
2017-07-12 17:50:08 +0300

    Skip stdc++ on Windows

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ from setuptools import Extension
 
 # Not all CPUs have march as a tuning parameter
 import platform
-cputune, llvm = ['-march=native'], []
+cputune, libraries, llvm = ['-march=native'], [], []
 if platform.machine() == 'ppc64le':
     cputune = ['-mcpu=native']
 if platform.system() == 'Darwin':
     llvm += ['-L/usr/local/opt/llvm/lib']
+if platform.system() == 'Windows':
+    libraries = []
+else:
+    libraries = ['stdc++']
 
 setuptools.setup(
     name='mrpt',
@@ -28,7 +32,7 @@ setuptools.setup(
             ],
             extra_compile_args=['-std=c++11', '-O3', '-ffast-math', '-s',
                                 '-fno-rtti', '-fopenmp', '-DNDEBUG'] + cputune,
-            libraries=['stdc++'],
+            libraries=libraries,
             extra_link_args=['-lgomp'] + llvm,
             include_dirs=['cpp/lib', numpy.get_include()]
         )
